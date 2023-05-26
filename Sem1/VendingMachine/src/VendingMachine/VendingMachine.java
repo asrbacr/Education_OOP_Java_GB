80164dc3c4aaab356767620ec1d796e3f48d5ae6
asrbacr <gek2372@ya.ru>
2023-05-26 19:27:03 +0300

    Update VendingMachine.java
    Update VendingMachine

--- Sem1/VendingMachine/src/VendingMachine/VendingMachine.java
+++ Sem1/VendingMachine/src/VendingMachine/VendingMachine.java
@@ -1,11 +1,8 @@
-package VendingMachine;
-
-import Domen.Product;
-
+package VendingMachines;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.plaf.ViewportUI;
+import Domen.Product;
 
 public class VendingMachine {
     private int volume;
@@ -13,27 +10,25 @@ public class VendingMachine {
 
     public VendingMachine(int volume) {
         this.volume = volume;
-        this.products = new ArrayList<Product>();
+        products = new ArrayList<Product>();
     }
-
     public int getVolume() {
         return volume;
     }
-
     public void setVolume(int volume) {
         this.volume = volume;
     }
-
     public List<Product> getProducts() {
         return products;
     }
-
     public void setProducts(List<Product> products) {
         this.products = products;
     }
 
-    public void addProduct(Product prod){
-
+    public void addProduct(Product prod)
+    {
         products.add(prod);
     }
+
+
 }
