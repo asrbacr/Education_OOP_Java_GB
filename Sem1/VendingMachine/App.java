8a564c6402e90c1bd57895ead6e14698986a503f
Vasiliy Gorulev <89159793029@ya.ru>
2023-05-26 19:32:50 +0300

    update structurs

---
+++ Sem1/VendingMachine/App.java
@@ -0,0 +1,23 @@
+import Domen.Bottle;
+import Domen.Product;
+import VendingMachines.VendingMachine;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Product item1 = new Product(1, "Lase", "Чипсы", 59);
+        item1.setPrice(98);
+        System.out.println(item1.toString());
+
+        Product item2 = new Bottle(2, "Colla", "Water", 70, 500);
+        
+        VendingMachine iMachine = new VendingMachine(300);
+        iMachine.addProduct(item1);
+        iMachine.addProduct(item2);
+        iMachine.addProduct(new Product(3, "Масло", "Масло", 599));
+        iMachine.addProduct(new Bottle(4, "Вода", "Whater", 599, 50));
+
+        for (Product prod : iMachine.getProducts()) {
+            System.out.println(prod.toString());
+        }
+    }
+}
