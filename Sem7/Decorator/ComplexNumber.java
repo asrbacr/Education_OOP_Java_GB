97e3bd9a72c06ea6ce3018b72ea8f4928359790f
Vasiliy Gorilev <89159793029@ya.ru>
2023-06-16 13:04:43 +0300

    делаю задание к семинару 7

---
+++ Sem7/Decorator/ComplexNumber.java
@@ -0,0 +1,10 @@
+package OOP_GB_4335.Sem7.Decorator;
+
+public class ComplexNumber {
+    private Integer real;
+    private Integer imaginary;
+
+    public String complexNumber(int real, int imaginary) {
+        return null;
+    }
+}
